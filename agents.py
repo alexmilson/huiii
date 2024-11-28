63b3c97f3ce44f45f4930c19bcbb6cafdc4b1e45
alexmilson <vg9176576@gmail.com>
2024-11-28 10:06:25 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -30,13 +30,13 @@ class ResearchCrewAgents:
         self.gemma_7b = ChatGroq(temperature=0.7, groq_api_key=os.environ.get("GROQ_API_KEY"), model_name="gemma-7b-it")
         
         # CHANGE YOUR MODEL HERE
-        self.selected_llm = self.gpt4
+        self.selected_llm = self.Mixtral_8x7B
     def researcher(self):
     # Detailed agent setup for the Research Expert
         return Agent(
         role='Expert',
-        goal='To break down broad visionary ideas into specific, actionable research topics, identify key areas requiring in-depth investigation, and prepare report that serves as a roadmap for future goals.',
-        backstory="You are an expert that can easily reconvey ideas from others through critical thinking and systems thinking to figure out what the visionary wants to accompolish",
+        goal='Identify the industry of the company and gather relevant product information.',
+        backstory="You are a research expert who can analyze web data to determine industry classification",
         verbose=True,
         allow_delegation=False,
         llm=self.selected_llm,
@@ -49,8 +49,8 @@ class ResearchCrewAgents:
         # Detailed agent setup for the Analyst
         return Agent(
             role='Analyst',
-            goal='Come up with a learning curriculumn that will allow for the visionary to reach deep and broad knowledge in order to accomplish their goals',
-            backstory="You are a talented organized logical educator who can deductively comeup with amazing learning plans in order to provide guidance starting from the goal and working backwards to the begining of a novice level so as to easily bridge the gap between inexperienced and experts alike.",
+            goal='Analyze industry trends and propose relevant AI/ML use cases for the company.',
+            backstory="You are an analyst skilled in identifying opportunities for AI and ML applications in various industries.",
             verbose=True,
             allow_delegation=False,
             llm=self.selected_llm,
@@ -63,8 +63,8 @@ class ResearchCrewAgents:
         # Detailed agent setup for the Writer
         return Agent(
             role='Technical writer',
-            goal='Use CrewAI tools to search and summarize findings of the previous agent, internet articles titles and their URLs, as well as books and online resource to carry out the learning needed',
-            backstory="You are organized course creater and talented educator that understands what it takes for beginners to get from point a to point be when it comes to learning, you export great findings, you are great at scraping the web links and resources geared to ward learning specific goals.",
+            goal='Use CrewAI tools to search and summarize findings of the previous agent, github links and their URLs, as well as kaggle dataset links and online resource to carry out the learning needed',
+            backstory="You are an expert in curating datasets and finding online resources that help in executing AI and ML solutions, you are great at scraping the web links and resources geared to ward learning specific goals.",
             verbose=True,
             allow_delegation=False,
             llm=self.selected_llm,
